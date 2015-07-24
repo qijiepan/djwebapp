8ac2f1e944695abbd7855f12e68c62d7390f7a01
qijiepan <qijiepan2014fall@gmail.com>
2015-07-24 12:24:52 -0400

    update blog

---
+++ djblog/blog/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+                       url(r"^$", "blog.views.index", name="index"),
+                       url(r"^post/(?P<pk>\d+)/$", "blog.views.post", name="post"),
+)
--- djblog/blog/views.py
+++ djblog/blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+from blog.models import Post
+
+def index(request):
+    
+
+    posts = Post.objects.all()
+    return render_to_response("blog/index.html",
+                  {"posts": posts},
+                  context_instance=RequestContext(request))
+
+def post(request, pk):
+    
+
+    post = get_object_or_404(Post, pk=pk)
+    return render_to_response("blog/post.html",
+                  {"post": post},
+                  context_instance=RequestContext(request))
